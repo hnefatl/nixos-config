3b568ed664f04b9dfb41a0c56a6688c960fd6a87
Keith Collister <hnefatl@gmail.com>
2025-05-26 16:45:38 +0100

    Add helper user for home assistant remote shutdown

--- users.nix
+++ users.nix
@@ -19,5 +19,27 @@
 
   # Allow these users to edit the nixos config files.
   users.groups.nixos.members = [ "keith" "root" ];
+
+  users.users."shutdown-helper" = {
+    isNormalUser = true;
+    openssh.authorizedKeys.keys = [
+      # HA for shutdown
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILzavfVQ9+CH89XqFLwIErHTExg4PoZmAON3D8zkJ9KE root@core-ssh"
+    ];
+  };
+  security.polkit = {
+    debug = true;
+    # Allow remote poweroff.
+    extraConfig = ''
+      polkit.addRule(function(action, subject) {
+	if (subject.user != "shutdown-helper") return polkit.Result.NOT_HANDLED;
+        if (action.id == "org.freedesktop.login1.power-off" ||
+            action.id == "org.freedesktop.login1.power-off-multiple-sessions") {
+          return polkit.Result.YES;
+        }
+        return polkit.Result.NOT_HANDLED;
+      });
+    '';
+  };
 }
 
