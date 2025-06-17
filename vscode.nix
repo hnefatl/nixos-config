1b05bf6abe409bf2149160c494b1df0a57860a0c
Keith Collister <hnefatl@gmail.com>
2025-06-17 21:47:50 +0100

    vscode extensions

--- vscode.nix
+++ vscode.nix
@@ -2,9 +2,13 @@
 
 (pkgs.vscode-with-extensions.override {
   vscodeExtensions = with pkgs.vscode-extensions; [
-    bbenoist.nix
     vscodevim.vim
+    bbenoist.nix
     ms-python.python
+    ms-pyright.pyright
+    ms-python.black-formatter
     ms-vscode-remote.remote-ssh
+    rust-lang.rust-analyzer
+    tamasfe.even-better-toml
   ];
 })
