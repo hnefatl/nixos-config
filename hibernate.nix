f7e939b4d03de7bde9208239697725bcdddc9772
Keith Collister <hnefatl@gmail.com>
2025-06-22 13:07:39 +0100

    Fix typo

--- hibernate.nix
+++ hibernate.nix
@@ -5,7 +5,7 @@ lib.mkIf (config.machine_config.instance == "desktop") {
   powerManagement.enable = true;
 
   services.logind = {
-    powerKey = if config.machine_config.form_factor == "desktop" then "suspend" else "hibernate";
+    powerKey = if config.machine_config.formFactor == "desktop" then "suspend" else "hibernate";
     powerKeyLongPress = "poweroff";
     # Idle detection here didn't seem to work in i3, instead added an exec to i3 config.
     # TODO: figure out what to do on Sway.
