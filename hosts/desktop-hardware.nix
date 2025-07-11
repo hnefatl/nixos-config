7077d3bd155666a062b8d2c14ee228096feb2d38
Keith Collister <hnefatl@gmail.com>
2025-07-11 20:58:42 +0100

    Standardise warthog samba secrets path

--- hosts/desktop-hardware.nix
+++ hosts/desktop-hardware.nix
@@ -6,7 +6,7 @@ let
     fsType = "cifs";
     options = let
       automount_opts = "x-systemd.automount,noauto,x-systemd.idle-timeout=60,x-systemd.device-timeout=5s,x-systemd.mount-timeout=5s";
-    in ["${automount_opts},credentials=/etc/nixos/warthog_samba_secrets,uid=1000,gid=100"];
+    in ["${automount_opts},credentials=/etc/nixos/secrets/warthog_samba,uid=1000,gid=100"];
   };
 in
 {
