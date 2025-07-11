ab59bfa0633b6f21ed677a3d144621151a8504b0
Keith Collister <hnefatl@gmail.com>
2025-07-11 19:54:44 +0100

    Add SSH for personal laptop to desktop

--- keith.nix
+++ keith.nix
@@ -8,7 +8,9 @@
     shell = pkgs.zsh;
 
     openssh.authorizedKeys.keys = lib.mkIf (config.machine_config.instance == "desktop") [
-      # Laptop
+      # Personal laptop
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJ46ZX6zJQrMOdffEZqJk5bbgZpTnaExEprMDS9aQUpa keith@laptop"
+      # Corp laptop
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIFwVu5wURjrYYBrXhuX1L/Bdi0fliXs1ldSI16QEHcjd kcollister@kcollister"
     ];
   };
