a2f543615d9007b4fe9f2b9f63d264018266ebfe
Keith Collister <hnefatl@gmail.com>
2025-07-12 18:55:03 +0100

    Split greetd into new file

--- os/configuration.nix
+++ os/configuration.nix
@@ -12,6 +12,7 @@
       #./wireguard.nix
       ./fingerprint.nix
       ./bluetooth.nix
+      ./greetd.nix
     ];
 
   nix = {
@@ -149,24 +150,6 @@
 
   # Useful for debugging available fonts.
   fonts.fontDir.enable = true;
-
-  services.greetd = {
-    enable = true;
-    settings = let
-      swayCommand = "${pkgs.sway}/bin/sway" + (if config.machine_config.instance == "desktop" then " --unsupported-gpu" else "");
-    in rec {
-      # By default show a nice login UI
-      default_session = {
-        command = "${pkgs.greetd.tuigreet}/bin/tuigreet --remember --time --cmd ${swayCommand}";
-        user = "greeter";
-      };
-      # If autologin is enabled, just jump straight in
-      initial_session = lib.mkIf config.machine_config.autoLogin {
-        command = swayCommand;
-        user = "keith";
-      };
-    };
-  };
 
   services.tlp.enable = config.machine_config.formFactor == "laptop";
 
