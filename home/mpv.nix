2e7b77c25bf6e6e623f37b9ac2922a48443e05c7
Keith Collister <hnefatl@gmail.com>
2025-08-14 22:21:53 +0100

    youtube-style keybindings for mpv

--- home/mpv.nix
+++ home/mpv.nix
@@ -9,6 +9,15 @@
       gpu-api = "opengl";
       ytdl-format = "bestvideo+bestaudio";
     };
+    bindings = {
+      # YouTube-style controls
+      "j" = "seek -10";
+      "k" = "cycle pause";
+      "l" = "seek 10";
+      "c" = "cycle sub";
+      "UP" = "add volume 5";
+      "DOWN" = "add volume -5";
+    };
   };
 
   xdg.mimeApps.defaultApplications = {
