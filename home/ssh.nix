a08b0642ca2f5bd1d63d48ab434458679b81f61a
Keith <hnefatl@gmail.com>
2025-08-21 14:09:27 +0100

    Add extra SSH hosts

--- home/ssh.nix
+++ home/ssh.nix
@@ -14,6 +14,31 @@
         hostname = "10.20.0.1";
         user = "root";
       };
+      "router_remote" = {
+        host = "router_remote";
+        hostname = "vpn.keith.collister.xyz";
+        user = "root";
+      };
+      "ap2" = {
+        host = "ap2";
+        hostname = "10.20.0.3";
+        user = "root";
+      };
+      "crash" = {
+        host = "crash";
+        hostname = "10.20.1.8";
+        user = "keith";
+      };
+      "parents_pi" = {
+        host = "parents_pi";
+        hostname = "10.29.0.8";
+        user = "root";
+      };
+      "octoprint" = {
+        host = "octoprint";
+        hostname = "10.20.1.7";
+        user = "pi";
+      };
     };
   };
   services.ssh-agent.enable = true;
