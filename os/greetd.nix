d91322b18e0af40abf93009bf72083b9ae24cd9b
Keith Collister <hnefatl@gmail.com>
2025-09-12 02:41:02 +0100

    Autoformat

--- os/greetd.nix
+++ os/greetd.nix
@@ -11,8 +11,7 @@
     settings =
       let
         swayCommand =
-          "${pkgs.sway}/bin/sway"
-          + (if config.machine_config.instance == "desktop" then " --unsupported-gpu" else "");
+          "${pkgs.sway}/bin/sway" + (if config.machine_config.instance == "desktop" then " --unsupported-gpu" else "");
       in
       rec {
         # By default show a nice login UI
