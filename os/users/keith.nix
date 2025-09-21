60194bcf3461b62a790bec518323503ac5446203
Keith Collister <hnefatl@gmail.com>
2025-09-21 15:17:40 +0100

    Don't gate SSH keys on machine instance.

--- os/users/keith.nix
+++ os/users/keith.nix
@@ -19,7 +19,7 @@
 
     shell = pkgs.zsh;
 
-    openssh.authorizedKeys.keys = lib.mkIf (config.machine_config.instance == "desktop") [
+    openssh.authorizedKeys.keys = [
       # Personal laptop
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJ46ZX6zJQrMOdffEZqJk5bbgZpTnaExEprMDS9aQUpa keith@laptop"
       # Corp laptop
