bb05a734b801dfd39d73c93809f5c4dfe50ddb9f
Keith Collister <hnefatl@gmail.com>
2025-09-22 21:41:47 +0100

    Add allowed signers for git

--- home/modules/git.nix
+++ home/modules/git.nix
@@ -9,8 +9,18 @@
     signing = {
       signByDefault = true;
       format = "ssh";
-      # Maybe not necessary?
-      #key = "${config.home.homeDirectory}/.ssh/id_ed25519.pub";
+      key = "${config.home.homeDirectory}/.ssh/id_ed25519.pub";
+    };
+    extraConfig = {
+      gpg.ssh.allowedSignersFile = "${config.home.homeDirectory}/.config/git/allowed_signers";
     };
   };
+
+  # At some point would be good to refactor these into a "user" config so can reference in
+  # both os/users/keith.nix and here.
+  home.file.".config/git/allowed_signers".text = ''
+    * ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJ46ZX6zJQrMOdffEZqJk5bbgZpTnaExEprMDS9aQUpa keith@laptop
+    * ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILVXUPyteZDsBXLsiFSVpW8Qr9qXi4wY7NkEQLeADAim keith@desktop
+    * ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIFwVu5wURjrYYBrXhuX1L/Bdi0fliXs1ldSI16QEHcjd kcollister@kcollister
+  '';
 }
