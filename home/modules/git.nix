0f53fdf00cb65bb5dbec6e20a84c80ff35b30faf
Keith Collister <hnefatl@gmail.com>
2025-09-22 21:44:09 +0100

    Add new warthog key to trusted signers.

--- home/modules/git.nix
+++ home/modules/git.nix
@@ -18,9 +18,12 @@
 
   # At some point would be good to refactor these into a "user" config so can reference in
   # both os/users/keith.nix and here.
+  # Want signing vs authorization keys? Since don't really want to be able to SSH using warthog keys?
+  # Maybe should just switch to GPG for signing keys? Could put in sops but then idk about root of trust.
   home.file.".config/git/allowed_signers".text = ''
     * ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJ46ZX6zJQrMOdffEZqJk5bbgZpTnaExEprMDS9aQUpa keith@laptop
     * ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILVXUPyteZDsBXLsiFSVpW8Qr9qXi4wY7NkEQLeADAim keith@desktop
+    * ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAII2sX9AdwN3BJEueF6T51VX75gNUc3MEw1+kc+Ak/PkI keith@warthog
     * ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIFwVu5wURjrYYBrXhuX1L/Bdi0fliXs1ldSI16QEHcjd kcollister@kcollister
   '';
 }
