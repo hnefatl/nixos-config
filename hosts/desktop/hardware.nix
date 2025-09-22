c83b5d8b85a5588563ec48eafc0d433997fa87bb
Keith Collister <hnefatl@gmail.com>
2025-09-22 19:20:07 +0100

    Restore random extra firmware packages, broke laptop. At some point should go through and see what's actually needed for each machine, might be able to trim boot time.

--- hosts/desktop/hardware.nix
+++ hosts/desktop/hardware.nix
@@ -1,4 +1,7 @@
+{ modulesPath, ... }:
 {
+  imports = [ (modulesPath + "/installer/scan/not-detected.nix") ];
+
   boot.initrd.availableKernelModules = [
     "xhci_pci"
     "ahci"
--- hosts/laptop/hardware.nix
+++ hosts/laptop/hardware.nix
@@ -1,4 +1,7 @@
+{ modulesPath, ...  }:
 {
+  imports = [ (modulesPath + "/installer/scan/not-detected.nix") ];
+
   boot.initrd.availableKernelModules = [
     "nvme"
     "xhci_pci"
