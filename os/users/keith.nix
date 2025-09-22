8d50379b6534025e4f192e7d05c54f6041dbdfa9
Keith Collister <hnefatl@gmail.com>
2025-09-22 21:05:57 +0100

    Add new PC SSH key

--- os/users/keith.nix
+++ os/users/keith.nix
@@ -20,8 +20,8 @@
     shell = pkgs.zsh;
 
     openssh.authorizedKeys.keys = [
-      # Personal laptop
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJ46ZX6zJQrMOdffEZqJk5bbgZpTnaExEprMDS9aQUpa keith@laptop"
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILVXUPyteZDsBXLsiFSVpW8Qr9qXi4wY7NkEQLeADAim keith@desktop"
       # Corp laptop
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIFwVu5wURjrYYBrXhuX1L/Bdi0fliXs1ldSI16QEHcjd kcollister@kcollister"
     ];
