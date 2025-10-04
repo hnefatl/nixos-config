8957e4f3e440883ee1efe13a88bbd85557a55085
Keith Collister <hnefatl@gmail.com>
2025-10-04 14:48:59 +0100

    Add netbootxyz and memtest86 to systemd boot menu.

--- os/boot/systemd.nix
+++ os/boot/systemd.nix
@@ -6,7 +6,11 @@
     initrd.systemd.enable = true;
 
     loader = {
-      systemd-boot.enable = true;
+      systemd-boot = {
+        enable = true;
+        netbootxyz.enable = true;
+        memtest86.enable = true;
+      };
       # KVM has a bit of latency, allow a bit longer.
       timeout = if config.machine_config.instance == "warthog" then 5 else 1;
     };
