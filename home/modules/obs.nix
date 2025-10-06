163d21a12ecc6483d3b91861dfec14b5b093da8a
Keith Collister <hnefatl@gmail.com>
2025-10-06 22:17:15 +0100

    Enable CUDA in OBS

--- home/modules/obs.nix
+++ home/modules/obs.nix
@@ -2,6 +2,11 @@
 {
   programs.obs-studio = {
     enable = true;
+    package = (
+      pkgs.obs-studio.override {
+        cudaSupport = true;
+      }
+    );
     plugins = with pkgs.obs-studio-plugins; [
       wlrobs
       obs-backgroundremoval
