7b9790fd84f53d3d588c2c8c67a632fa66f30132
Keith Collister <hnefatl@gmail.com>
2025-10-09 22:42:01 +0100

    Tweaks to standard filesystem logic

--- hosts/standard-filesystems.nix
+++ hosts/standard-filesystems.nix
@@ -4,8 +4,6 @@
 { lib, config, ... }:
 let
   cfg = config.standard_filesystems;
-  root_pool = "zroot";
-  key_pool = "zfskeys";
 in
 {
   options.standard_filesystems = {
@@ -14,13 +12,25 @@ in
       default = "4G";
     };
     partuuids = {
-      ${key_pool} = lib.mkOption {
+      zfskeys = lib.mkOption {
         type = lib.types.str;
       };
       swap = lib.mkOption {
         type = lib.types.str;
       };
       boot = lib.mkOption {
+        type = lib.types.str;
+      };
+    };
+    pool_names = {
+      root = lib.mkOption {
+        default = "zroot";
+        readOnly = true;
+        type = lib.types.str;
+      };
+      zfskeys = lib.mkOption {
+        default = "zfskeys";
+        readOnly = true;
         type = lib.types.str;
       };
     };
@@ -38,7 +48,7 @@ in
         after = [ "cryptsetup.target" ];
         # Finish loading keys to the pools before starting the ZFS mounts
         before = [
-          "zfs-import-${root_pool}.service"
+          "zfs-import-${cfg.pool_names.root}.service"
           "sysroot.mount"
         ];
         unitConfig.DefaultDependencies = "no";
@@ -51,10 +61,10 @@ in
         # is and allows easier mounting if new keys are needed.
         script = ''
           echo "Loading bootstrap keys"
-          zpool import ${key_pool}
-          zpool scrub -w ${key_pool}
+          zpool import ${cfg.pool_names.zfskeys}
+          zpool scrub -w ${cfg.pool_names.zfskeys}
           mkdir /zfskeys
-          mount -t zfs -o ro ${key_pool} /zfskeys
+          mount -t zfs -o ro ${cfg.pool_names.zfskeys} /zfskeys
 
           echo "Loading ZFS pools and keys"
           zpool import -a
@@ -62,7 +72,7 @@ in
 
           echo "Unloading bootstrap keys"
           umount /zfskeys
-          zpool export ${key_pool}
+          zpool export ${cfg.pool_names.zfskeys}
         '';
       };
     };
@@ -80,18 +90,19 @@ in
       };
 
       "/" = {
-        device = "${root_pool}/enc/snap/root";
+        # Overriden in impermanent setups.
+        device = "${cfg.pool_names.root}/enc/snap/root";
         fsType = "zfs";
         neededForBoot = true;
       };
 
       "/home/keith" = {
-        device = "${root_pool}/enc/snap/home/keith";
+        device = "${cfg.pool_names.root}/enc/snap/home/keith";
         fsType = "zfs";
       };
 
       "/nix" = {
-        device = "${root_pool}/enc/snap/nix";
+        device = "${cfg.pool_names.root}/enc/snap/nix";
         fsType = "zfs";
       };
 
@@ -107,59 +118,74 @@ in
 
     # There's quite a few steps to manually mount the above filesystems. Write helper scripts
     # prefilled with the right UUIDs to the unencrypted easy-to-access /boot partition.
-    systemd.services.writeBootRecoveryScript = {
-      script = ''
-        tee /boot/mount_recovery.sh <<EOF
-        if (( \$EUID != 0 )); then
-            echo "Not root"
-            exit
-        fi
-        set -e
-        set -x
+    #
+    # Ideally would use a systemd oneshot service, which runs on every activation. However,
+    # these scripts are most useful on first boot after `nixos-install`, which would be before
+    # systemd has had a chance to run to create them, so for this specific usecase activationScripts
+    # is better.
+    system.activationScripts.writeBootRecoveryScript = {
+      text =
+        let
+          # Convert attrsets like:
+          #   "/nix" = { device = "foo/nix"; }
+          # into:
+          #   "mount -tzfs foo/nix /os/nix"
+          # for the specific volumes we care about.
+          volumesToMount = [
+            "/"
+            "/nix"
+            "/home/keith"
+            "/boot"
+          ];
+          requiredMounts = lib.attrsets.filterAttrs (p: _: builtins.elem p volumesToMount) config.fileSystems;
+          toMountCommand = mountpoint: attrs: "mount -tzfs ${attrs.device} /os${mountpoint}";
+          mountCommands = lib.strings.concatLines (lib.attrsets.mapAttrsToList toMountCommand requiredMounts);
+        in
+        ''
+          cat > /boot/mount_recovery.sh <<EOF
+          if (( \$EUID != 0 )); then
+              echo "Not root"
+              exit
+          fi
+          set -e
+          set -x
 
-        # Unlock the LUKS volume containing ZFS keys.
-        systemd-cryptsetup attach ${key_pool} '/dev/disk/by-partuuid/${cfg.partuuids.zfskeys}'
-        zpool import ${key_pool}
-        # Make sure the partition is healthy.
-        zpool scrub -w ${key_pool}
-        mkdir -p /zfskeys
-        mount -t zfs -o ro ${key_pool} /zfskeys
+          # Unlock the LUKS volume containing ZFS keys.
+          systemd-cryptsetup attach ${cfg.pool_names.zfskeys} '/dev/disk/by-partuuid/${cfg.partuuids.zfskeys}'
+          zpool import ${cfg.pool_names.zfskeys}
+          # Make sure the partition is healthy.
+          zpool scrub -w ${cfg.pool_names.zfskeys}
+          mkdir -p /zfskeys
+          mount -t zfs -o ro ${cfg.pool_names.zfskeys} /zfskeys
 
-        # Import all the other pools.
-        zpool import -a -f
-        zfs load-key -a
+          # Import all the other pools.
+          zpool import -a -f
+          zfs load-key -a
 
-        # Configure everything on /os
-        mkdir -p /os
-        mount -t zfs ${root_pool}/enc/snap/root /os
-        mount -t zfs ${root_pool}/enc/snap/home/keith /os/home/keith
-        mount -t zfs ${root_pool}/enc/snap/nix /os/nix
-        mount '/dev/disk/by-partuuid/${cfg.partuuids.boot}' /os/boot
-        echo 'System mounted on /os'
-        EOF
+          # Mount everything on /os
+          mkdir -p /os
+          ${mountCommands}
+          echo 'System mounted on /os'
+          EOF
 
-        tee /boot/unmount_recovery.sh <<EOF
-        if (( \$EUID != 0 )); then
-            echo "Not root"
-            exit
-        fi
-        set -e
-        set -x
+          cat > /boot/unmount_recovery.sh <<EOF
+          if (( \$EUID != 0 )); then
+              echo "Not root"
+              exit
+          fi
+          set -e
+          set -x
 
-        umount /os/boot
-        # This handles unmounting any datasets that are mounted.
-        # We need to export when we're done so that on next boot the OS can import them.
-        zpool export -a
-        systemd-cryptsetup detach ${key_pool}
-        rmdir /os /zfskeys
-        EOF
+          umount /os/boot
+          # This handles unmounting any datasets that are mounted.
+          # We need to export when we're done so that on next boot the OS can import them.
+          zpool export -a
+          systemd-cryptsetup detach ${cfg.pool_names.zfskeys}
+          rmdir /os /zfskeys
+          EOF
 
-        chmod u+x /boot/mount_recovery.sh /boot/unmount_recovery.sh
-      '';
-      wantedBy = [ "multi-user.target" ];
-      serviceConfig = {
-        Type = "oneshot";
-      };
+          chmod u+x /boot/mount_recovery.sh /boot/unmount_recovery.sh
+        '';
     };
   };
 }
