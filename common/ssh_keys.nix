fd1cb3aa5370fe59b3979578ba5a840bc709203f
Keith Collister <hnefatl@gmail.com>
2025-10-09 23:14:46 +0100

    New warthog ssh signing key

--- common/ssh_keys.nix
+++ common/ssh_keys.nix
@@ -2,7 +2,7 @@
   keith = {
     laptop = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJ46ZX6zJQrMOdffEZqJk5bbgZpTnaExEprMDS9aQUpa keith@laptop";
     desktop = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILVXUPyteZDsBXLsiFSVpW8Qr9qXi4wY7NkEQLeADAim keith@desktop";
-    warthog = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINH2aSRfXbucOu8ZxBUFEGv8BfeYG0ZOsHfVJcznpwKY keith@warthog";
+    warthog = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIH9Fy/pN77dfMrER6gnP+cnxNdn2J/eJQ2NDzPWrGvPN keith@warthog";
     corp-laptop = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIFwVu5wURjrYYBrXhuX1L/Bdi0fliXs1ldSI16QEHcjd kcollister@kcollister";
   };
 }
