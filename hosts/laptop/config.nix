fcded8f7ae14a03bd7a8f3c530674f09bc5ec0f7
Keith Collister <hnefatl@gmail.com>
2025-10-22 20:03:27 +0100

    Pin laptop kernel more stably.

--- hosts/laptop/config.nix
+++ hosts/laptop/config.nix
@@ -1,3 +1,4 @@
+{ pkgs, lib, ... }:
 {
   imports = [
     ./model.nix
@@ -5,6 +6,11 @@
     ./filesystems.nix
     ./networking.nix
   ];
+
+  # TODO: remove once NixOS selects a new LTS kernel as default (pkgs.linuxPackages.kernel.version is an LTS release).
+  # Personal laptop's wifi chip driver has a bug preventing suspend/hibernate in currently-stable 6.12.
+  # The chosen kernel version may need to be bumped to a more recent kernel if it goes out of support before the next LTS release.
+  boot.kernelPackages = lib.mkIf (lib.versionOlder pkgs.linuxPackages.kernel.version "6.13") pkgs.linuxPackages_6_16;
 
   # DO NOT CHANGE: original NixOS version, for backcompat decisions.
   system.stateVersion = "24.11"; # Did you read the comment?
--- os/boot/lanzaboote.nix
+++ os/boot/lanzaboote.nix
@@ -9,12 +9,6 @@
   imports = [ ./systemd.nix ];
 
   boot = {
-    # TODO: remove once no-op.
-    # Personal laptop's wifi chip driver has a bug preventing suspend/hibernate in currently-stable 6.12.
-    # Enforce oldest kernel 6.14.
-    kernelPackages = lib.mkIf (
-      config.machine_config.instance == "laptop" && lib.versionOlder pkgs.linuxPackages.kernel.version "6.13"
-    ) pkgs.pkgs.linuxPackages_latest;
     initrd.systemd.enable = true;
 
     # Replaced by lanzaboote
