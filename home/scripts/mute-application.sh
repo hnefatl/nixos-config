2b7750dad36e8cbfa44fbe8a1d922f206d851b85
Keith Collister <hnefatl@gmail.com>
2026-01-27 20:08:59 +0000

    Better notifications for mute

--- home/scripts/mute-application.sh
+++ home/scripts/mute-application.sh
@@ -8,4 +8,19 @@ for index in ${indices} ; do
     pactl set-sink-input-mute "${index}" toggle
     output=$(pactl get-sink-input-mute "${index}")
 done
-exec notify-send "Application ${pid}" "${output}"
+
+id_file="/tmp/mute-application-${pid}"
+existing_id=()
+if [[ -f "${id_file}" ]] ; then
+    existing_id=(-r "$(< "${id_file}")")
+fi
+if [[ "${output}" =~ "yes" ]] ; then
+    urgency=(-u critical)
+    timeout=(-t 0)
+    body="Muted"
+else
+    urgency=(-u normal)
+    timeout=(-t 1000)
+    body="Unmuted"
+fi
+exec notify-send "${urgency[@]}" "${timeout[@]}" "${existing_id[@]}" -p "Application ${pid}" "${body}" > "${id_file}"
--- home/scripts/mute-source.sh
+++ home/scripts/mute-source.sh
@@ -4,4 +4,19 @@ source=$(pactl get-default-source)
 description=$(pactl -f json list sources | jq -r ".[] | select(.name == \"${source}\") | .properties.\"device.description\"")
 
 pactl set-source-mute "${source}" toggle
-exec notify-send "${description}" "$(pactl get-source-mute "${source}")"
+
+id_file=/tmp/mute-source-id
+existing_id=()
+if [[ -f "${id_file}" ]] ; then
+    existing_id=(-r "$(< "${id_file}")")
+fi
+if [[ "$(pactl get-source-mute "${source}")" =~ "yes" ]] ; then
+    urgency=(-u critical)
+    timeout=(-t 0)
+    body="Muted"
+else
+    urgency=(-u normal)
+    timeout=(-t 1000)
+    body="Unmuted"
+fi
+exec notify-send "${urgency[@]}" "${timeout[@]}" "${existing_id[@]}" -p "${description}" "${body}" > "${id_file}"
