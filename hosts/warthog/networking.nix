e47607639f1bbd3b31a6e1aab13b5c67e0684b2b
Keith Collister <hnefatl@gmail.com>
2026-02-07 21:56:41 +0000

    Fix warthog dns resolution

--- hosts/warthog/networking.nix
+++ hosts/warthog/networking.nix
@@ -1,16 +1,20 @@
 {
+  networking = {
+    nameservers = [ "10.20.0.1" ];
+    # Resolving e.g. `laptop.lan` doesn't work with `dig` but does with `resolvectl query`
+    # which I think is all that matters? Seems to be working okay for prometheus at least.
+    search = [ "lan" ];
+  };
+
   systemd.network = {
     enable = true;
     networks."10-lan" = {
       matchConfig.Name = "eno1";
-      address = [
-        "10.20.1.3/16"
-      ];
-      routes = [
-        { Gateway = "10.20.0.1"; }
-      ];
+      address = [ "10.20.1.3/16" ];
+      gateway = [ "10.20.0.1" ];
       linkConfig.RequiredForOnline = "routable";
     };
   };
+
   networking.useDHCP = false;
 }
