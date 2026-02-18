7a995917a6af09a61c0ed9bd5dd4e06b1b3ef177
Keith Collister <hnefatl@gmail.com>
2026-02-19 07:43:47 +0800

    Minor refactor

--- os/modules/warthog-nfs.nix
+++ os/modules/warthog-nfs.nix
@@ -1,3 +1,13 @@
+let
+  options = [
+    "noauto"
+    "x-systemd.automount"
+    "x-systemd.idle-timeout=600"
+    "x-systemd.device-timeout=5s"
+    "x-systemd.mount-timeout=5s"
+    "noatime"
+  ];
+in
 {
   boot.supportedFilesystems = [ "nfs" ];
 
@@ -5,73 +15,31 @@
   fileSystems."/warthog/backup" = {
     device = "10.20.1.3:/pool/backup";
     fsType = "nfs";
-    options = [
-      "noauto"
-      "x-systemd.automount"
-      "x-systemd.idle-timeout=600"
-      "x-systemd.device-timeout=5s"
-      "x-systemd.mount-timeout=5s"
-      "noatime"
-    ];
+    inherit options;
   };
   fileSystems."/warthog/media" = {
     device = "10.20.1.3:/pool/media";
     fsType = "nfs";
-    options = [
-      "noauto"
-      "x-systemd.automount"
-      "x-systemd.idle-timeout=600"
-      "x-systemd.device-timeout=5s"
-      "x-systemd.mount-timeout=5s"
-      "noatime"
-    ];
+    inherit options;
   };
   fileSystems."/warthog/old_disks" = {
     device = "10.20.1.3:/pool/old_disks";
     fsType = "nfs";
-    options = [
-      "noauto"
-      "x-systemd.automount"
-      "x-systemd.idle-timeout=600"
-      "x-systemd.device-timeout=5s"
-      "x-systemd.mount-timeout=5s"
-      "noatime"
-    ];
+    inherit options;
   };
   fileSystems."/warthog/services" = {
     device = "10.20.1.3:/pool/services";
     fsType = "nfs";
-    options = [
-      "noauto"
-      "x-systemd.automount"
-      "x-systemd.idle-timeout=600"
-      "x-systemd.device-timeout=5s"
-      "x-systemd.mount-timeout=5s"
-      "noatime"
-    ];
+    inherit options;
   };
   fileSystems."/warthog/transfer" = {
     device = "10.20.1.3:/pool/transfer";
     fsType = "nfs";
-    options = [
-      "noauto"
-      "x-systemd.automount"
-      "x-systemd.idle-timeout=600"
-      "x-systemd.device-timeout=5s"
-      "x-systemd.mount-timeout=5s"
-      "noatime"
-    ];
+    inherit options;
   };
   fileSystems."/warthog/camera" = {
     device = "10.20.1.3:/pool/camera";
     fsType = "nfs";
-    options = [
-      "noauto"
-      "x-systemd.automount"
-      "x-systemd.idle-timeout=3600"
-      "x-systemd.device-timeout=5s"
-      "x-systemd.mount-timeout=5s"
-      "noatime"
-    ];
+    inherit options;
   };
 }
