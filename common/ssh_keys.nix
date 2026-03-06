871f791cc2253f8a4cb14e8668d0da893b256222
Keith <hnefatl@gmail.com>
2026-03-06 10:11:15 +0000

    Update to new corp SSH key

--- common/ssh_keys.nix
+++ common/ssh_keys.nix
@@ -4,7 +4,7 @@
     laptop = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJ46ZX6zJQrMOdffEZqJk5bbgZpTnaExEprMDS9aQUpa keith@laptop";
     desktop = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILVXUPyteZDsBXLsiFSVpW8Qr9qXi4wY7NkEQLeADAim keith@desktop";
     warthog = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIH9Fy/pN77dfMrER6gnP+cnxNdn2J/eJQ2NDzPWrGvPN keith@warthog";
-    corp-laptop = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIFwVu5wURjrYYBrXhuX1L/Bdi0fliXs1ldSI16QEHcjd kcollister@kcollister";
+    corp-laptop = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIEk5aeLDN+cxW7ARfrOda3tDiLRAEm6nu4GUEyXwKuZL kcollister@kcollister2";
   };
   # Host keys (for known hosts checking)
   hosts = {
