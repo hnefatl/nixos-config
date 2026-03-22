2a29f2b9bb135c1d82c8ab5f487768759e4a50b7
Keith Collister <hnefatl@gmail.com>
2026-03-22 09:12:58 +0000

    Fix laptop+workdesk monitor layout

--- home/modules/sway/sway.nix
+++ home/modules/sway/sway.nix
@@ -53,9 +53,13 @@
         position = "2560,0";
         transform = "90";
       };
+      # Laptop screen always at 0,0
+      output."BOE NE135A1M-NY1 Unknown" = {
+        position = "0,0";
+      };
       # Desk screen
       output."Dell Inc. DELL P2720DC 81WTK0131RMS" = {
-        position = "-320,-1440";
+        position = "-560,-1440";
       };
 
       window = {
