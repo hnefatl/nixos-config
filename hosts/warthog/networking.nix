15185512653220b4de4df15b27d8c990344f88b0
Keith Collister <hnefatl@gmail.com>
2026-05-04 00:37:20 +0100

    Switch warthog to dhcp

--- hosts/warthog/networking.nix
+++ hosts/warthog/networking.nix
@@ -1,20 +1,14 @@
 {
-  networking = {
-    nameservers = [ "10.20.0.1" ];
-    # Resolving e.g. `laptop.lan` doesn't work with `dig` but does with `resolvectl query`
-    # which I think is all that matters? Seems to be working okay for prometheus at least.
-    search = [ "lan" ];
-  };
+  networking.useDHCP = false;
 
   systemd.network = {
     enable = true;
     networks."10-lan" = {
       matchConfig.Name = "eno1";
-      address = [ "10.20.1.3/16" ];
-      gateway = [ "10.20.0.1" ];
+      DHCP = "ipv4";
       linkConfig.RequiredForOnline = "routable";
     };
   };
-
-  networking.useDHCP = false;
+  # Enable DNS lookups
+  services.resolved.enable = true;
 }
