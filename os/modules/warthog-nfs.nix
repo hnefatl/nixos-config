fde5be146d8ca1cf49612b2122656f89868f72ac
Keith Collister <hnefatl@gmail.com>
2026-05-09 00:33:23 +0100

    Update warthog nfs clients to use ipv6 addr

--- os/modules/warthog-nfs.nix
+++ os/modules/warthog-nfs.nix
@@ -13,32 +13,32 @@ in
 
   # TODO: sync with the server nixos definition somehow.
   fileSystems."/warthog/backup" = {
-    device = "10.20.1.3:/pool/backup";
+    device = "[2a01:4b00:bd20:7010:230:59ff:fe28:d000]:/pool/backup";
     fsType = "nfs";
     inherit options;
   };
   fileSystems."/warthog/media" = {
-    device = "10.20.1.3:/pool/media";
+    device = "[2a01:4b00:bd20:7010:230:59ff:fe28:d000]:/pool/media";
     fsType = "nfs";
     inherit options;
   };
   fileSystems."/warthog/old_disks" = {
-    device = "10.20.1.3:/pool/old_disks";
+    device = "[2a01:4b00:bd20:7010:230:59ff:fe28:d000]:/pool/old_disks";
     fsType = "nfs";
     inherit options;
   };
   fileSystems."/warthog/services" = {
-    device = "10.20.1.3:/pool/services";
+    device = "[2a01:4b00:bd20:7010:230:59ff:fe28:d000]:/pool/services";
     fsType = "nfs";
     inherit options;
   };
   fileSystems."/warthog/transfer" = {
-    device = "10.20.1.3:/pool/transfer";
+    device = "[2a01:4b00:bd20:7010:230:59ff:fe28:d000]:/pool/transfer";
     fsType = "nfs";
     inherit options;
   };
   fileSystems."/warthog/camera" = {
-    device = "10.20.1.3:/pool/camera";
+    device = "[2a01:4b00:bd20:7010:230:59ff:fe28:d000]:/pool/camera";
     fsType = "nfs";
     inherit options;
   };
