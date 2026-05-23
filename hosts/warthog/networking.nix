57e17a3d4712cf550adf4605d2e933cad364f9d2
Keith Collister <hnefatl@gmail.com>
2026-05-23 23:22:38 +0100

    Update warthog networking

--- hosts/warthog/networking.nix
+++ hosts/warthog/networking.nix
@@ -1,12 +1,18 @@
 {
+  # Disable legacy stack.
   networking.useDHCP = false;
 
   systemd.network = {
     enable = true;
+
     networks."10-lan" = {
       matchConfig.Name = "eno1";
+      # DHCP for ipv4, SLAAC for ipv6.
       DHCP = "ipv4";
-      networkConfig.MulticastDNS = true;
+      networkConfig = {
+        MulticastDNS = true;
+        IPv6AcceptRA = true;
+      };
       linkConfig.RequiredForOnline = "routable";
     };
   };
