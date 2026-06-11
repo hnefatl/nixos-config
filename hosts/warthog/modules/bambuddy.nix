4d1bb681c3c468a6a25e35cd42f1588db4e1272d
Keith Collister <hnefatl@gmail.com>
2026-06-11 20:57:52 +0100

    Open bambuddy ports for virtual printer

--- hosts/warthog/modules/bambuddy.nix
+++ hosts/warthog/modules/bambuddy.nix
@@ -1,3 +1,4 @@
+{ lib, ... }:
 {
   users.users.bambuddy = {
     isSystemUser  = true;
@@ -6,5 +7,31 @@
   };
   users.groups.bambuddy = {
     gid = 2000;
+  };
+
+  networking.firewall = {
+    # https://wiki.bambuddy.cool/features/virtual-printer/#required-ports
+    allowedTCPPorts = [
+      3000
+      3002
+      8883
+      6000
+      322
+      990
+      50000
+      50001
+      50002
+      50003
+      50004
+      50005
+      50006
+      50007
+      50008
+      50009
+    ];
+    #] ++ (lib.range 50000 50029);
+    allowedUDPPorts = [
+      2021
+    ];
   };
 }
